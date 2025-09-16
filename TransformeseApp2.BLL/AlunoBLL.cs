1c7c60b873123093f05f0ac6b259d56f133c57d9
luan.coesposito <luan.coesposito@SMP0581321W10-1>
2025-09-15 22:24:07 -0300

    feat: CadastrarUnidades and ListarUnidades

--- TransformeseApp2.BLL/AlunoBLL.cs
+++ TransformeseApp2.BLL/AlunoBLL.cs
@@ -1,12 +1,20 @@
-﻿using TransformeseApp2.DTO;
+﻿using TransformeseApp2.DAL;
+using TransformeseApp2.DTO;
 
 namespace TransformeseApp2.BLL
 {
     public class AlunoBLL
     {
-        public void CadastrarAluno()
+        public void CadastrarAluno(AlunoDTO alunoDTO)
         {
+            //Validação antes de salvar o aluno
+            if (string.IsNullOrWhiteSpace(alunoDTO.Nome))
+                throw new Exception("Nome é obrigatório");
 
+            Database.Alunos.Add(alunoDTO);
         }
+
+        public List<AlunoDTO> ListarAlunos() => Database.Alunos;
+
     }
 }
---
+++ TransformeseApp2.BLL/UnidadeBLL.cs
@@ -0,0 +1,25 @@
+﻿using TransformeseApp2.DAL;
+using TransformeseApp2.DTO;
+
+namespace TransformeseApp2.BLL
+{
+    public class UnidadeBLL
+    {
+        private List<UnidadeDTO> _unidades = Database.Unidades;
+        public void CadastrarUnidade(UnidadeDTO unidadeDTO)
+        {
+            //Validação antes de salvar a unidade
+            if (string.IsNullOrWhiteSpace(unidadeDTO.Nome))
+                throw new Exception("Nome é obrigatório");
+
+            Database.Unidades.Add(unidadeDTO);
+        }
+
+        public List<UnidadeDTO> ListarUnidades() => Database.Unidades;
+
+        public UnidadeDTO GetById(int id)
+        {
+            return _unidades.FirstOrDefault(unidade => unidade.Id == id);
+        }
+    }
+}
