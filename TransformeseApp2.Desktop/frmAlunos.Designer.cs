a7410285abadb340390681bef0a45e4ca4f86194
luan.coesposito <luan.coesposito@SMP0581321W10-1>
2025-09-22 19:20:41 -0300

    feat: atualização tela alunos

--- TransformeseApp2.Desktop/frmAlunos.Designer.cs
+++ TransformeseApp2.Desktop/frmAlunos.Designer.cs
@@ -28,18 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtNome = new TextBox();
+            cboCurso = new ComboBox();
+            cboUnidade = new ComboBox();
+            btnCadastrar = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            dgAlunos = new DataGridView();
+            txtBusca = new TextBox();
+            btnBusca = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dgAlunos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(19, 131);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(100, 23);
+            txtNome.TabIndex = 0;
+            // 
+            // cboCurso
+            // 
+            cboCurso.FormattingEnabled = true;
+            cboCurso.Location = new Point(22, 186);
+            cboCurso.Name = "cboCurso";
+            cboCurso.Size = new Size(97, 23);
+            cboCurso.TabIndex = 1;
+            // 
+            // cboUnidade
+            // 
+            cboUnidade.FormattingEnabled = true;
+            cboUnidade.Location = new Point(22, 239);
+            cboUnidade.Name = "cboUnidade";
+            cboUnidade.Size = new Size(97, 23);
+            cboUnidade.TabIndex = 1;
+            // 
+            // btnCadastrar
+            // 
+            btnCadastrar.BackColor = Color.DarkGreen;
+            btnCadastrar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCadastrar.ForeColor = Color.Transparent;
+            btnCadastrar.Location = new Point(22, 268);
+            btnCadastrar.Name = "btnCadastrar";
+            btnCadastrar.Size = new Size(97, 38);
+            btnCadastrar.TabIndex = 2;
+            btnCadastrar.Text = "Cadastrar";
+            btnCadastrar.UseVisualStyleBackColor = false;
+            btnCadastrar.Click += btnCadastrar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 168);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Curso:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 221);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Unidade:";
+            // 
+            // dgAlunos
+            // 
+            dgAlunos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgAlunos.Location = new Point(125, 131);
+            dgAlunos.Name = "dgAlunos";
+            dgAlunos.Size = new Size(262, 175);
+            dgAlunos.TabIndex = 4;
+            // 
+            // txtBusca
+            // 
+            txtBusca.Location = new Point(125, 102);
+            txtBusca.Name = "txtBusca";
+            txtBusca.Size = new Size(179, 23);
+            txtBusca.TabIndex = 0;
+            // 
+            // btnBusca
+            // 
+            btnBusca.BackColor = Color.DodgerBlue;
+            btnBusca.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBusca.ForeColor = Color.Transparent;
+            btnBusca.Location = new Point(310, 96);
+            btnBusca.Name = "btnBusca";
+            btnBusca.Size = new Size(77, 32);
+            btnBusca.TabIndex = 2;
+            btnBusca.Text = "Pesquisar";
+            btnBusca.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.logo;
+            pictureBox1.Location = new Point(24, 19);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(363, 71);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
             // 
             // frmAlunos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(399, 330);
+            Controls.Add(pictureBox1);
+            Controls.Add(dgAlunos);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnBusca);
+            Controls.Add(btnCadastrar);
+            Controls.Add(cboUnidade);
+            Controls.Add(cboCurso);
+            Controls.Add(txtBusca);
+            Controls.Add(txtNome);
             Name = "frmAlunos";
             Text = "frmAlunos";
+            ((System.ComponentModel.ISupportInitialize)dgAlunos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtNome;
+        private ComboBox cboCurso;
+        private ComboBox cboUnidade;
+        private Button btnCadastrar;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private DataGridView dgAlunos;
+        private TextBox txtBusca;
+        private Button btnBusca;
+        private PictureBox pictureBox1;
     }
 }
--- TransformeseApp2.Desktop/frmAlunos.cs
+++ TransformeseApp2.Desktop/frmAlunos.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TransformeseApp2.DTO;
 
 namespace TransformeseApp2.Desktop
 {
@@ -15,6 +16,19 @@ namespace TransformeseApp2.Desktop
         public frmAlunos()
         {
             InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var aluno = new AlunoDTO { };
+            }
+            catch (Exception erro)
+            {
+
+                throw;
+            }
         }
     }
 }
